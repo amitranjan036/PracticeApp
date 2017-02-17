63d277636b546bb25ac572d7bb92a4fb3d7fe92f
amitranjan036 <amitranjan036@gmail.com>
2017-02-17 18:30:48 +0530

    launch the app on click is done

--- app/src/main/java/com/greedygame/samplelist/MainActivity1.java
+++ app/src/main/java/com/greedygame/samplelist/MainActivity1.java
@@ -1,10 +1,13 @@
 package com.greedygame.samplelist;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import java.util.ArrayList;
@@ -15,22 +18,34 @@ import java.util.List;
  */
 
 public class MainActivity1 extends AppCompatActivity {
-    ArrayList<String> innerArray = new ArrayList<String>();
-    ArrayList<String> outerArray = new ArrayList<String>();
+    ArrayList<String> duplicateappnameArray = new ArrayList<String>();
+    ArrayList<String> originalappnameArray = new ArrayList<String>();
+    ArrayList<String> packagenameArray = new ArrayList<String>();
     ArrayAdapter<String> adapter;
     ListView list2;
     List<ApplicationInfo> packages;
     ProgressDialog pd;
     String appname;
+    PackageManager pm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main1);
 
+          pm = getPackageManager();
           pd =new ProgressDialog(MainActivity1.this);
           list2 = (ListView) findViewById(R.id.listview1);
-          adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, outerArray);
+          adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, originalappnameArray);
           list2.setAdapter(adapter);
+          list2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+              @Override
+              public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+              {
+                  Intent launchIntent = pm.getLaunchIntentForPackage(packagenameArray.get(position));
+                  startActivity(launchIntent);
+              }
+          });
           new Numberlist().execute(null,null,null);
       }
 
@@ -38,16 +53,20 @@ public class MainActivity1 extends AppCompatActivity {
       {
           @Override
           protected Void doInBackground(Void... params) {
-              final PackageManager pm = getPackageManager();
+
               packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
               for (ApplicationInfo info : packages)
                 {
                     appname = pm.getApplicationLabel(info).toString();
-                    innerArray.add(appname);
+                    duplicateappnameArray.add(appname);
+                    packagenameArray.add(info.packageName);
                 }
-            try {
+            try
+            {
                 Thread.sleep(2000);
-            } catch (InterruptedException e) {
+            }
+            catch (InterruptedException e)
+            {
                 e.printStackTrace();
             }
 
@@ -64,9 +83,10 @@ public class MainActivity1 extends AppCompatActivity {
         @Override
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
-            outerArray.addAll(innerArray);
+            originalappnameArray.addAll(duplicateappnameArray);
             adapter.notifyDataSetChanged();
             pd.dismiss();
+
         }
 
     }
